b646947ae3002d88deec09ed6d6c829c6ad37673
aslakgun <59713737+aslakgun@users.noreply.github.com>
2020-04-04 18:37:15 -0700

    first commit

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, 
+AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading',{static: true})  header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+  constructor() { 
+    console.log('constructor called!');
+  }
+  
+  ngOnChanges (changes: SimpleChanges){
+    console.log('ngOnChanges called!');
+    console.log(this.header.nativeElement.textContent);
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!')
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!')
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!')
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!')
+}
+ngOnDestroy(){
+  console.log('ngOnDestroy called!')
+}
+}
